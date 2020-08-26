85cbe20bb496d3aa9626cb9f7f67adba0b0d3eb1
williamning <61674789+williamning@users.noreply.github.com>
2020-08-26 16:11:56 +0800

    ‘前端组件化（三）：抽象出公共组件类

---
+++ demo03/Component.js
@@ -0,0 +1,31 @@
+class Component {
+    setState (state) {
+        const oldEl = this.el
+        this.state = state
+        this._renderDOM()
+        if (this.onStateChange) this.onStateChange(oldEl, this.el)
+    }
+
+    _renderDOM () {
+        this.el = createDOMFromString(this.render())
+        if (this.onClick) {
+        this.el.addEventListener('click', this.onClick.bind(this), false)
+        }
+        return this.el
+    }
+}
+
+// ::String => ::Document //DOM 事件的 API 只有 DOM 结构才能用。[不能往一个字符串里面添加事件]
+const createDOMFromString = (domString) => {
+    const div = document.createElement('div')
+    div.innerHTML = domString
+    return div
+}
+
+const mount = (component, wrapper) => {
+    wrapper.appendChild(component._renderDOM())
+    component.onStateChange = (oldEl, newEl) => {
+        wrapper.insertBefore(newEl, oldEl)
+        wrapper.removeChild(oldEl)
+    }
+}
---
+++ demo03/likeButton.js
@@ -0,0 +1,24 @@
+class LikeButton extends Component {
+    constructor () {
+      super()
+      this.state = { isLiked: false }
+    }
+
+    onClick () {
+      this.setState({
+        isLiked: !this.state.isLiked
+      })
+    }
+
+    render () {
+      return `
+        <button class='like-btn'>
+          <span class='like-text'>${this.state.isLiked ? '取消' : '点赞'}</span>
+          <span>👍</span>
+        </button>
+      `
+    }
+  }
+
+const wrapper = document.querySelector('.wrapper')
+mount(new LikeButton(), wrapper)
